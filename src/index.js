a21384239348c97be76043429ce09383dd11af8e
freeda <seekwell@gmail.com>
2019-03-02 01:21:13 +0300

    [=] js

--- src/index.js
+++ src/index.js
@@ -1,103 +1,52 @@
 import './scss/main.scss'
-import getCoords from './js/coords'
 import helper from './js/helper'
+import handlers from './js/handlers'
 
-(function() {
-  var throttle = function(type, name, obj) {
-    obj = obj || window;
-    var running = false;
-    var func = function() {
-      if (running) { return; }
-      running = true;
-      requestAnimationFrame(function() {
-        obj.dispatchEvent(new CustomEvent(name));
-        running = false;
-      });
-    };
-    obj.addEventListener(type, func);
+(function(type, name, obj) {
+  obj = obj || window;
+  var running = false;
+  var func = function() {
+    if (running) { return; }
+    running = true;
+    requestAnimationFrame(function() {
+      obj.dispatchEvent(new CustomEvent(name));
+      running = false;
+    });
   };
+  obj.addEventListener(type, func);
+})("resize", "optimizedResize");
 
-  throttle("resize", "optimizedResize");
+var run = function() {
+  var form = document.getElementById('form')
+  var input = document.getElementById('domain')
+  var output = document.getElementById('output')
+  var row = document.getElementById('row')
+  var list = row.children
 
+  helper.replaceClassName(list)
+  helper.changeHeight(list)
 
-  var run = function() {
-    var form = document.getElementById('form')
-    var input = document.getElementById('domain')
-    var output = document.getElementById('output')
-    var parent = document.getElementById('row')
-    var list = document.getElementById('row').children
-    var opened = []
+  // handle event
 
-    var handler = (e) => {
-      parent.removeEventListener('click', handler)
+  form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    formSubmitHandler(e, input, info, output)
+  })
 
-      var target = e.target
-      var id = target.id || target.parentNode.id
-      var el = document.getElementById(id)
-      let prom = new Promise((resolve) => {
-
-        if (opened.length) {
-          helper.hide(opened[0])
-          setTimeout(() => { resolve(opened.pop().id) }, 200)
-        } else {
-          resolve({})
-        }
-
-      }).then((idx) => {
-
-        if ((id === idx) || id == 'row') {return}
-
-        var pos = getCoords(el)
-        var hint = document.getElementById(`${id}Hint`)
-        var o = {
-          id: id,
-          el: el,
-          list: list,
-          hint: hint,
-          corner: hint.children[1],
-          maxHeight: helper.calculateMaxHeight(parent.children) - 20,
-          heightHint: hint.clientHeight,
-          top: pos.top,
-          left: pos.left
-        }
-
-        helper.show(o)
-        opened.push(o)
-
-      }).then(()=>{
-        setTimeout(()=>{
-          parent.addEventListener("click", handler)
-        }, 200)
-      })
+  row.addEventListener("click", {
+    handleEvent: function (e) {
+      row.removeEventListener(e.type, this, false);
+      handlers.iconsHandler(e, row, list)
     }
+  })
 
-    helper.replaceClassName(list)
-    helper.changeHeight(list)
+  window.addEventListener("optimizedResize", function() {
+      setTimeout(() => {
+        helper.replaceClassName(list)
+        helper.changeHeight(list)
+        helper.changePropertiesHint(handlers.stack[0])
+      }, 400)
+  })
+}
 
-    // handle event
-    form.addEventListener("submit", function(e) {
-      e.preventDefault()
-      var val = input.value
-      if (val.length) {
-        info.style.visibility = ''
-        if (val.match(/[.]ru/) === null) {
-          val += '.ru'
-        }
-        input.value = val
-        output.innerHTML = val
-      } else {
-        info.style.visibility = 'hidden'
-      }
-    })
-    parent.addEventListener("click", handler)
-    window.addEventListener("optimizedResize", function() {
-        setTimeout(() => {
-          helper.replaceClassName(list)
-          helper.changeHeight(list)
-          helper.changePropertiesHint(opened[0])
-        }, 400)
-    })
-  }
-
-  document.addEventListener("DOMContentLoaded", run);
-})();
+document.addEventListener("DOMContentLoaded", run);
---
+++ src/js/handlers.js
@@ -0,0 +1,76 @@
+helper = require('./helper')
+getCoords = require('./coords')
+
+this.stack = []
+
+var iconsHandler = function (e, parent, list, stack) {
+  var stack = stack || this.stack
+  var target = e.target
+  var id = target.id || target.parentNode.id
+  var el = document.getElementById(id)
+
+  var prom = new Promise((resolve) => {
+
+    if (stack.length) {
+      helper.hide(stack[0])
+      setTimeout(() => {
+        resolve(stack.pop().id)
+      }, 200)
+    } else {
+      resolve({})
+    }
+
+  }).then((idx) => {
+
+    if ((id === idx) || (id === 'row') || stack.length) {return stack}
+
+    var pos = getCoords(el)
+    var hint = document.getElementById(`${id}Hint`)
+    var o = {
+      id: id,
+      el: el,
+      list: list,
+      hint: hint,
+      corner: hint.children[1],
+      maxHeight: helper.calculateMaxHeight(parent.children) - 20,
+      heightHint: hint.clientHeight,
+      top: pos.top,
+      left: pos.left
+    }
+
+    helper.show(o)
+    stack.push(o)
+    return stack
+
+  }).then((stack)=>{
+    setTimeout(()=>{
+      parent.addEventListener("click", {
+        handleEvent: function (e) {
+          parent.removeEventListener(e.type, this, false);
+          iconsHandler(e, parent, list, stack)
+        }
+      })
+    }, 200)
+  })
+}
+
+var formSubmitHandler = function (e, input, info, output) {
+  var val = input.value
+  if (val.length) {
+    info.style.visibility = ''
+    if (val.match(/[.]ru/) === null) {
+      val += '.ru'
+    }
+    input.value = val
+    output.innerHTML = val
+  } else {
+    info.style.visibility = 'hidden'
+  }
+}
+
+
+module.exports = {
+  stack: this.stack,
+  iconsHandler: iconsHandler,
+  formSubmitHandler: formSubmitHandler
+}
